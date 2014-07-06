1e4509a1889848920e3faa2e074a9b3649ac8f5e
luok <kevin.luo91@gmail.com>
2014-07-06 15:52:43 -0700

    initial commit

---
+++ Focus/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Focus
+//
+//  Created by Kai Luo on 7/6/14.
+//  Copyright (c) 2014 Kai Luo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
